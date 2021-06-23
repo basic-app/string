ca619a20ec84ceda2151a53b9976609922d85faf
basic-app <dev@basic-app.com>
2021-06-23 15:06:04 +0300

    n/a

--- Helpers/string_helper.php
+++ Helpers/string_helper.php
@@ -8,7 +8,7 @@ use BasicApp\String\StringHelper;
 
 if (!function_exists('string_starts_with'))
 {
-    public function string_starts_with(string $haystack, string $needle) : bool
+    function string_starts_with(string $haystack, string $needle) : bool
     {
         return StringHelper::startsWith($haystack, $needle);
     }
@@ -16,7 +16,7 @@ if (!function_exists('string_starts_with'))
 
 if (!function_exists('string_ends_with'))
 {
-    public function string_ends_with(string $haystack, string $needle) : bool
+    function string_ends_with(string $haystack, string $needle) : bool
     {
         return StringHelper::endsWith($haystack, $needle);
     }
@@ -24,7 +24,7 @@ if (!function_exists('string_ends_with'))
 
 if (!function_exists('string_quote'))
 {
-    public function string_quote(string $value) : string
+    function string_quote(string $value) : string
     {
         return StringHelper::quote($value);
     }
@@ -32,7 +32,7 @@ if (!function_exists('string_quote'))
 
 if (!function_exists('string_is'))
 {
-    public function string_is($pattern, $value, &$matches = null, $flags = null) : bool
+    function string_is($pattern, $value, &$matches = null, $flags = null) : bool
     {
         return StringHelper::is($pattern, $value, $matches, $flags);
     }
@@ -40,7 +40,7 @@ if (!function_exists('string_is'))
 
 if (!function_exists('string_replace_first'))
 {
-    public function string_replace_first(string $search, string $replace, string $subject) : string
+    function string_replace_first(string $search, string $replace, string $subject) : string
     {
         return StringHelper::replaceFirst($search, $replace, $subject);
     }
@@ -48,7 +48,7 @@ if (!function_exists('string_replace_first'))
 
 if (!function_exists('string_replace_last'))
 {
-    public function string_replace_last(string $search, string $replace, string $subject) : string
+    function string_replace_last(string $search, string $replace, string $subject) : string
     {
         return StringHelper::replaceLast($search, $replace, $subject);
     }
